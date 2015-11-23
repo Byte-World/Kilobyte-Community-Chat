94e8321f11052995d152bd5cbcf303a120349807
StigCoder04 <stevenhe04@live.com>
2015-11-22 19:46:11 -0800

    User Logs and new login.

--- client/main.js
+++ client/main.js
@@ -1,13 +1,58 @@
 var socket = io();
 var username;
+var page;
+var userList;
 
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val(), username);
+  socket.emit('chat message', $('#inputMssg').val(), username);
   $('#inputMssg').val('');
   return false;
+});
+
+$('#enter').click(function () {
+  keystate[13];
+  checkEnter();
 });
 
 socket.on('chat message', function(msg, username){
   var stringMssg = username + ": " + msg;
   $('#messages').append($('<li>').text(stringMssg));
 });
+
+socket.on('logUserEnterRoom' function(username) {
+  var log = username + " has entered the chat room.";
+  clientlog(log)
+});
+
+function signIn() {
+  page = "logIn"
+
+  document.addEventListener("keydown", function (event) {
+    keystate[event.keyCode] = true;
+  });
+
+  document.addEventListener("keyup", function (event) {
+    delete keystate[event.keyCode];
+  });
+  $(".loginArea").show();
+  $(".chatArea").hide();
+  var i;
+  for (i=10; i > 5; i++) {
+    checkEnter();
+  }
+}
+
+function checkEnter() {
+  if (keystate[13]) {
+    page = "chatRooms";
+    var username = $('#inputMssg').val();
+    i = 2;
+    $(".loginArea").hide();
+    $(".chatArea").show();
+    socket.emit('newUser', username);
+  }
+}
+
+function clientLog(logMessage) {
+  $.('#messages').append($('<li class="logMssg">').text(logMessage));
+}
--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3000;
+var users = [];
+var adminCodes = ["abc123", "321cba"]
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  io.emit('chat message', msg);
+});
+
+  socket.on('newUser' function(username) {
+    users.push(username);
+    socket.emit('logUserEnterRoom', username);
   });
 });
 

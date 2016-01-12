f7309bdfd9cc7e50510df0d86aac332bc66c0f96
StigCoder04 <stevenhe04@live.com>
2016-01-11 20:36:23 -0800

    Added Files

---
+++ fileOpener/fileOpener/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace fileOpener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1479, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(139, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1328, 629);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Brush";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(64, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1479, 672);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ fileOpener/fileOpener/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fileOpener
+{
+    public class cord
+    {
+        public int X;
+        public int Y;
+
+        public cord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += Form1_MouseMove;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.Click += pictureBox1_Click;
+        }
+
+        void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cord cursor = new cord(e.X, e.Y);
+
+            string text = "x = " + cursor.X + ", y = " + cursor.Y;
+            label1.Text = text;
+
+            if (picBoxClick == true)
+            {
+                int selectedIn = comboBox1.SelectedIndex;
+                int brushSize = sizes[selectedIn];
+                drawCurrentMarkerSpotCir(cursor, brushSize);
+            }
+
+            if (picMsUp == true)
+            {
+                picBoxClick = false;
+            }
+            else
+            {
+
+            }
+            
+        }
+
+        void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cord cursor = new cord(e.X, e.Y);
+
+            string text = "x = " + cursor.X + ", y = " + cursor.Y;
+            label1.Text = text;
+        }
+
+        public System.Drawing.SolidBrush whiteSol = new System.Drawing.SolidBrush(Color.White);
+        public System.Drawing.Graphics graphics;
+        public System.Drawing.SolidBrush blackSol = new System.Drawing.SolidBrush(Color.Black);
+        public bool picBoxClick = false;
+        public bool picMsUp = true;
+        public int[] sizes = new int[10];
+        public bool firstMark = true;
+        public cord prevCord = new cord(0, 0);
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            graphics = pictureBox1.CreateGraphics();
+            graphics.FillRectangle(whiteSol, 0, 0, 2000, 2000);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string currentSet;
+                int x = i + 1;
+                currentSet = x.ToString();
+
+                comboBox1.Items.Add(currentSet);
+            }
+        }
+
+        public void drawCurrentMarkerSpotCir(cord curPos, int sizePX)
+        {
+            int radius = sizePX / 2;
+            graphics = pictureBox1.CreateGraphics();
+            graphics.FillEllipse(blackSol, curPos.X + radius, curPos.Y + radius, sizePX, sizePX);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            /*
+            picBoxClick = true;
+            picMsUp = false;
+            */
+        }
+
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            picBoxClick = true;
+            picMsUp = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                sizes[i] = i + 1;
+            }
+        }
+
+        void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            picMsUp = true;
+        }
+    }
+}

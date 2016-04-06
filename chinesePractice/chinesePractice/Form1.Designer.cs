ee50b9212c83a9d5ac6479e3d3580807bc971278
StigCoder04 <stevenhe04@live.com>
2016-04-05 20:12:28 -0700

    Code commit for 4/5/2016

--- chinesePractice/chinesePractice/Form1.Designer.cs
+++ chinesePractice/chinesePractice/Form1.Designer.cs
@@ -41,15 +41,28 @@
             this.inputChar = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.testInput = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.testInput = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.findStatistics = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -118,7 +131,7 @@
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.inputChar);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(373, 117);
+            this.panel2.Location = new System.Drawing.Point(392, 44);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(275, 124);
             this.panel2.TabIndex = 1;
@@ -187,29 +200,37 @@
             this.panel3.Controls.Add(this.label7);
             this.panel3.Controls.Add(this.label6);
             this.panel3.Controls.Add(this.label5);
-            this.panel3.Location = new System.Drawing.Point(100, 319);
+            this.panel3.Location = new System.Drawing.Point(370, 184);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(322, 124);
             this.panel3.TabIndex = 2;
             // 
-            // label5
+            // button2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Constantia", 11F);
-            this.label5.Location = new System.Drawing.Point(4, 4);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 23);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Test Mode";
+            this.button2.Location = new System.Drawing.Point(239, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label6
+            // testInput
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 31);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(255, 17);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Type in the correct pin yin for the word:";
+            this.testInput.Font = new System.Drawing.Font("Calibri", 10F);
+            this.testInput.Location = new System.Drawing.Point(73, 76);
+            this.testInput.Name = "testInput";
+            this.testInput.Size = new System.Drawing.Size(160, 28);
+            this.testInput.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Answer:";
             // 
             // label7
             // 
@@ -221,43 +242,155 @@
             this.label7.TabIndex = 2;
             this.label7.Text = "a";
             // 
-            // label8
+            // label6
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 79);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(58, 17);
-            this.label8.TabIndex = 3;
-            this.label8.Text = "Answer:";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(255, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Type in the correct pin yin for the word:";
             // 
-            // testInput
+            // label5
             // 
-            this.testInput.Font = new System.Drawing.Font("Calibri", 10F);
-            this.testInput.Location = new System.Drawing.Point(73, 76);
-            this.testInput.Name = "testInput";
-            this.testInput.Size = new System.Drawing.Size(160, 28);
-            this.testInput.TabIndex = 4;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 11F);
+            this.label5.Location = new System.Drawing.Point(4, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Test Mode";
             // 
-            // button2
+            // panel4
             // 
-            this.button2.Location = new System.Drawing.Point(239, 81);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Submit";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label17);
+            this.panel4.Controls.Add(this.label16);
+            this.panel4.Controls.Add(this.label15);
+            this.panel4.Controls.Add(this.label14);
+            this.panel4.Controls.Add(this.label13);
+            this.panel4.Controls.Add(this.label12);
+            this.panel4.Controls.Add(this.label11);
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.findStatistics);
+            this.panel4.Controls.Add(this.dateTimePicker1);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Location = new System.Drawing.Point(404, 361);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(288, 177);
+            this.panel4.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Constantia", 11F);
+            this.label9.Location = new System.Drawing.Point(4, 4);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 23);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Statistics";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // findStatistics
+            // 
+            this.findStatistics.Location = new System.Drawing.Point(3, 59);
+            this.findStatistics.Name = "findStatistics";
+            this.findStatistics.Size = new System.Drawing.Size(117, 23);
+            this.findStatistics.TabIndex = 2;
+            this.findStatistics.Text = "Find Statistics";
+            this.findStatistics.UseVisualStyleBackColor = true;
+            this.findStatistics.Click += new System.EventHandler(this.findStatistics_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 17);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Problems Attempted:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(149, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 17);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "N/A";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(4, 110);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 17);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Correct:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(4, 131);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 17);
+            this.label13.TabIndex = 6;
+            this.label13.Text = "Incorrect:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(8, 155);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(84, 17);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "Time Spent:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(66, 110);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(31, 17);
+            this.label15.TabIndex = 8;
+            this.label15.Text = "N/A";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(77, 131);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(31, 17);
+            this.label16.TabIndex = 9;
+            this.label16.Text = "N/A";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(98, 155);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(31, 17);
+            this.label17.TabIndex = 10;
+            this.label17.Text = "N/A";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1074, 576);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Chinese Practice";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -265,6 +398,8 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -290,6 +425,18 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button findStatistics;
     }
 }
 
--- chinesePractice/chinesePractice/Form1.cs
+++ chinesePractice/chinesePractice/Form1.cs
@@ -12,6 +12,49 @@ namespace chinesePractice
 {
     public partial class Form1 : Form
     {
+        public class Stats
+        {
+            public int month;
+            public int day;
+            public int year;
+            public int problemsAttempted;
+            public int correct;
+            public int incorrect;
+            public int minutes;
+            public int hours;
+
+            public Stats()
+            {
+                month = new int();
+                day = new int();
+                year = new int();
+                problemsAttempted = new int();
+                correct = new int();
+                incorrect = new int();
+                minutes = new int();
+                hours = new int();
+            }
+
+            public Stats(int Month, int Day, int Year, int ProblemsAttempted, int Correct, int Incorrect, int Minutes, int Hours)
+            {
+                month = new int();
+                day = new int();
+                year = new int();
+                problemsAttempted = new int();
+                correct = new int();
+                incorrect = new int();
+                minutes = new int();
+                hours = new int();
+                month = Month;
+                day = Day;
+                year = Year;
+                problemsAttempted = ProblemsAttempted;
+                correct = Correct;
+                incorrect = Incorrect;
+                minutes = Minutes;
+                hours = Hours;
+            }
+        }
         public class Question
         {
             public string chineseCharacter;
@@ -57,7 +100,8 @@ namespace chinesePractice
 
             inputChar.Text = "";
             inputPinYin.Text = "";
-            setCurrentQuestion(a, b);
+            //setCurrentQuestion(a, b);
+
         }
 
         public void setCurrentQuestion(string insertCharacter, string insertAnswer)
@@ -76,6 +120,25 @@ namespace chinesePractice
             {
                 System.Windows.Forms.MessageBox.Show("Wrong." + " The answer was: " + currentQuestion.pinYin);
             }
+        }
+
+        public void changeMode(int menuNo)
+        {
+
+        }
+
+
+        private void findStatistics_Click(object sender, EventArgs e)
+        {
+            Stats currentStats = new Stats();
+        }
+
+        public Stats retrieveStats(Stats partialStats)
+        {
+            Stats completeStats = new Stats();
+
+
+            return completeStats;
         }
     }
 }

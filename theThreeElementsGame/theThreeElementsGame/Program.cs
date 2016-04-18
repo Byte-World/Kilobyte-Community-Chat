4990c4ae6063d2d3982f639f5228e630d806b642
StigCoder04 <stevenhe04@live.com>
2016-04-17 19:47:59 -0700

    Code commit for 4/17/2016

---
+++ theThreeElementsGame/theThreeElementsGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace theThreeElementsGame
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+
+        public class specialAbility
+        {
+            int ID;
+            int abilityDescription;
+
+            public specialAbility()
+            {
+
+            }
+        }
+
+        public class character
+        {
+            string characterName;
+            string characterImagePath;
+            int startingHealth;
+            int currentHealth;
+            int dmgPerAttack;
+            int specialAbilityID;
+            Boolean characterAlive;
+
+            public character()
+            {
+                characterAlive = true;
+            }
+
+            public character(string characterNameParam, string characterImagePathParam, int startingHealthParam, int dmgPerAttackParam, int specialAbilityIDParam)
+            {
+                characterName = characterNameParam;
+                characterImagePath = characterNameParam;
+                startingHealth = startingHealthParam;
+                currentHealth = startingHealthParam;
+                dmgPerAttack = dmgPerAttackParam;
+                specialAbilityID = specialAbilityIDParam;
+                characterAlive = true;
+            }
+
+            public void subtractHealth(int healthToSubtract)
+            {
+                if (healthToSubtract > currentHealth)
+                {
+                    currentHealth = 0;
+                    characterAlive = false;
+                }
+                else
+                {
+                    currentHealth = currentHealth - healthToSubtract;
+                }
+            }
+        }
+    }
+}
